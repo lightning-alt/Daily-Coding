a37eccbe6f14aad9bb7e13e460d58e4608fc44cf
lightning-alt <lightningofficial06@gmail.com>
2026-09-23 20:55:21 +0530

    Add a simple Java calculator example

---
+++ SimpleCalculator.java
@@ -0,0 +1,11 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int firstNumber = 12;
+        int secondNumber = 4;
+
+        System.out.println("Sum: " + (firstNumber + secondNumber));
+        System.out.println("Difference: " + (firstNumber - secondNumber));
+        System.out.println("Product: " + (firstNumber * secondNumber));
+        System.out.println("Quotient: " + (firstNumber / secondNumber));
+    }
+}
